4d07925cc2b5f50b80fa3d1099c519a7b21c4942
Kateryna <kati.skrinnikova@gmail.com>
2024-09-07 17:19:14 +0300

    span status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLable, { Status } from './components/StatusLabel'
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Hello, world!</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1>Home Page</h1>
+            <StatusLable status={Status.Active}>Active</StatusLable>
+            <StatusLable status={Status.NotActive}>Not active</StatusLable>
+            <StatusLable status={Status.Pending}>Pending</StatusLable>
+            <StatusLable status={Status.Suspended}>Suspended</StatusLable>
+        </main>
+    )
 }
